4ef661e06dbf7765213aade1a17d7b3506dd265e
Ramana Nelapaltal <rama_70@yahoo.com>
2022-06-28 07:44:14 +0530

    change in the test.java files

--- AutomationGit/src/main/java/firstPK/test.java
+++ AutomationGit/src/main/java/firstPK/test.java
@@ -1,23 +1,24 @@
 package firstPK;
 
+import java.util.Scanner;
+
 public class test {
 
-    public static void printloop(){
-        for(int i=0;i<100;i++){
-            System.out.println("value of i " +i);
+    public static void printloop() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("value of i " + i);
         }
     }
 
     public static void main(String arg[]) {
 
         System.out.println("Welcome to git repo");
+        System.out.println("welocome to java");
     }
-  
-  public static void gettable(int table) {
-    Scanner sc  = new Scanner(System.in);
-    System.out.println("enter number");
-  }
-  
-  public static void main(String ar[]){
-    System.out.println("welocome to java");
+
+    public static void gettable(int table) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number");
+    }
+
 }
