be26aa1294acae8b2c0dd5d77872dc3d34c29b54
ramana <rmnelapatla@users.noreply.github.com>
2022-06-28 07:36:31 +0530

    Update test.java
    add few line

--- AutomationGit/src/main/java/firstPK/test.java
+++ AutomationGit/src/main/java/firstPK/test.java
@@ -1,4 +1,12 @@
 package firstPK;
 
 public class test {
+  
+  public static void gettable(int table) {
+    Scanner sc  = new Scanner(System.in);
+    System.out.println("enter number");
+  }
+  
+  public static void main(String ar[]){
+    System.out.println("welocome to java");
 }
