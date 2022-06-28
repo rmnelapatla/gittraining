d64d0ac1302b41e38ccd503a025ffa02ce7ba400
Ramana Nelapaltal <rama_70@yahoo.com>
2022-06-28 07:39:38 +0530

    change in the test.java files

--- AutomationGit/src/main/java/firstPK/test.java
+++ AutomationGit/src/main/java/firstPK/test.java
@@ -1,4 +1,15 @@
 package firstPK;
 
 public class test {
+
+    public static void printloop(){
+        for(int i=0;i<100;i++){
+            System.out.println("value of i " +i);
+        }
+    }
+
+    public static void main(String arg[]) {
+
+        System.out.println("Welcome to git repo");
+    }
 }
