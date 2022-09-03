4b8795a21a2ea8a366479e5355efdd82dd439808
ramana <rmnelapatla@users.noreply.github.com>
2022-09-03 06:51:05 +0530

    Update Test1234.java

--- AutomationGit/src/main/java/firstPK/Test1234.java
+++ AutomationGit/src/main/java/firstPK/Test1234.java
@@ -5,5 +5,9 @@ public class Test1234 {
 
     public static void main(String ar[]){
         System.out.println(" in the grid");
+        
+        int b =9;
+        System.out.println("print some thing");
+        
     }
 }
