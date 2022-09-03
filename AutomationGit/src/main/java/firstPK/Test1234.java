720df2a100d89ca4ffc9a24f6f2ba7a6be47c6ce
ramana <rmnelapatla@users.noreply.github.com>
2022-09-03 06:46:13 +0530

    Update Test1234.java
    change the code for new req

--- AutomationGit/src/main/java/firstPK/Test1234.java
+++ AutomationGit/src/main/java/firstPK/Test1234.java
@@ -5,5 +5,8 @@ public class Test1234 {
 
     public static void main(String ar[]){
         System.out.println(" in the grid");
+        int a=7;
+        int b =8;
+        System.out.println(a/b);
     }
 }
