6209c936451c7abe8a699729f57346eb8bfcaf43
rmnelapatla70 <rama_70@yahoo.com>
2022-09-19 10:32:25 +0530

    Remove

--- AutomationGit/src/main/java/firstPK/Automation.java
+++ AutomationGit/src/main/java/firstPK/Automation.java
@@ -2,29 +2,7 @@ package firstPK;
 
 public class Automation {
 public static void main(String ar[]) {
-  
-  /**
-  
-  
-  
-  **/
-  
-  system.out.println("fdsfjkdsfjkdjkfdfjd");
-  
-  
-  int age =0;
-  
-  
-  Boolean b = true;
-  
-  
-  system.out.println("fdsfjkdsfjkdjkfdfjd");
 
-    System.out.println("lkfjdskfjdskljdklfjds");
-    int a =0;
-
-
-    String b = "fdfsdhfjdhfjdfhjdf";
 }
 
 }
