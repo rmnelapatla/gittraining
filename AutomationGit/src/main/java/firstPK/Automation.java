ea4db983b27fec26b8d7a7462e9ec30ebdd44bb9
rmnelapatla70 <rama_70@yahoo.com>
2022-09-19 10:29:50 +0530

    new code added

--- AutomationGit/src/main/java/firstPK/Automation.java
+++ AutomationGit/src/main/java/firstPK/Automation.java
@@ -3,6 +3,11 @@ package firstPK;
 public class Automation {
 public static void main(String ar[]) {
 
+    System.out.println("lkfjdskfjdskljdklfjds");
+    int a =0;
+
+
+    String b = "fdfsdhfjdhfjdfhjdf";
 }
 
 }
