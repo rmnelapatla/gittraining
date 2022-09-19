b513d60dcbf27e80927930d8d0e2bdb70c058e7d
ramana <rmnelapatla@users.noreply.github.com>
2022-09-19 10:29:08 +0530

    Update Automation.java

--- AutomationGit/src/main/java/firstPK/Automation.java
+++ AutomationGit/src/main/java/firstPK/Automation.java
@@ -2,6 +2,23 @@ package firstPK;
 
 public class Automation {
 public static void main(String ar[]) {
+  
+  /**
+  
+  
+  
+  **/
+  
+  system.out.println("fdsfjkdsfjkdjkfdfjd");
+  
+  
+  int age =0;
+  
+  
+  Boolean b = true;
+  
+  
+  system.out.println("fdsfjkdsfjkdjkfdfjd");
 
 }
 
